da74cd392d85c8196c16224334837065b62b4072
0qy <linqianye333@gmail.com>
2021-09-23 17:47:35 +1200

    add env example and google api key

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
-const express = require('express');
-const axios = require('axios');
+import express, { Express} from 'express';
+import axios from 'axios';
 
-const apiKey='AIzaSyCvZbp0Koj7sfqFQBu-dBEyQMH7tJZL9As';
+const apiKey = process.env.API_KEY
 const baseApiURl = 'https://www.googleapis.com/youtube/v3'
 
-const app = express();
-const port = 3000;
+const app: Express = express();
+const PORT: string | number = 3000;
+
 
 app.get('/', (req, res) => {
     res.send('hello');
@@ -14,13 +15,11 @@ app.get('/', (req, res) => {
 app.get('/search', async (req, res) => {
     const url = `${baseApiURl}/search?key=${apiKey}&type=video&part=snippet&q=cats`;
     const response = await axios.get(url);
-    const titles = response.data.items.map((item) => item.snippet.title);
-    console.log(titles);
+    const titles = response.data.items.map((item: any) => item.snippet.title);
     res.send(titles);
-    console.log('response', response);
 })
 
-app.listen(port, () => {
+app.listen(PORT, () => {
     console.log('App has started, listening on port 3000');
 })
 
