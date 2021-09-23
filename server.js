f661dd1c13d0544c760ee131ce09db88df463c5f
0qy <linqianye333@gmail.com>
2021-09-23 17:27:40 +1200

    server js

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const axios = require('axios');
+
+const apiKey='AIzaSyCvZbp0Koj7sfqFQBu-dBEyQMH7tJZL9As';
+const baseApiURl = 'https://www.googleapis.com/youtube/v3'
+
 const app = express();
 const port = 3000;
 
@@ -6,6 +11,16 @@ app.get('/', (req, res) => {
     res.send('hello');
 });
 
-app.listen(port, () => {
-    console.log('App is starting');
+app.get('/search', async (req, res) => {
+    const url = `${baseApiURl}/search?key=${apiKey}&type=video&part=snippet&q=cats`;
+    const response = await axios.get(url);
+    const titles = response.data.items.map((item) => item.snippet.title);
+    console.log(titles);
+    res.send(titles);
+    console.log('response', response);
 })
+
+app.listen(port, () => {
+    console.log('App has started, listening on port 3000');
+})
+
