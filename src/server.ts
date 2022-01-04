04631009ec6d463cd986f5089cd9d2ce49c68960
QY <linqianye@tutanota.com>
2022-01-05 09:53:02 +1300

    ts prettier config

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,41 @@
-import express, { Express} from 'express';
-import axios from 'axios';
+import express, { Express } from 'express';
+import { google, youtube_v3 } from 'googleapis';
 
-const apiKey = process.env.API_KEY
-const baseApiURl = 'https://www.googleapis.com/youtube/v3'
+const apiKey = process.env.API_KEY;
+const date = new Date();
+const stringDate = date.toISOString();
+
+const youtube: youtube_v3.Youtube = google.youtube({
+  version: 'v3',
+  auth: apiKey,
+});
 
 const app: Express = express();
 const PORT: string | number = 3000;
 
-
 app.get('/', (req, res) => {
-    res.send('hello');
+  res.send('hello');
 });
 
-app.get('/search', async (req, res) => {
-    const url = `${baseApiURl}/search?key=${apiKey}&type=video&part=snippet&q=cats`;
-    const response = await axios.get(url);
-    const titles = response.data.items.map((item: any) => item.snippet.title);
+app.get('/search', async (req, res, next) => {
+  console.log(apiKey);
+  try {
+    const listParams: youtube_v3.Params$Resource$Search$List = {
+      part: ['snippets'],
+      publishedAfter: stringDate,
+      type: ['video'],
+    };
+    const response = await youtube.search.list(listParams);
+
+    console.log(response);
+    const titles =
+      response && response?.data?.items?.map((item: any) => item.snippet.title);
     res.send(titles);
-})
+  } catch (err) {
+    next(err);
+  }
+});
 
 app.listen(PORT, () => {
-    console.log('App has started, listening on port 3000');
-})
-
+  console.log('App has started, listening on port 3000');
+});
